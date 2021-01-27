141270d5e4ca3a509dd220c9ac234e40ecd680f3
ArmoredVortex <rachitpandey.gghs@gmail.com>
2021-01-27 12:17:56 +0530

    Updated sample.env, randomised stuff

--- commands/dare.js
+++ commands/dare.js
@@ -2,7 +2,7 @@ const {MessageEmbed} = require('discord.js')
 module.exports = {
     name : 'dare',
     execute(client,message,args){
-        let question = client.dares[Math.floor(Math.random()*client.dares.length)]
+        let question = client.dares[Math.floor(Math.random() * client.dares.length)]
         const embed = new MessageEmbed()
         .setTitle('Dare')
         .setDescription(question)
--- commands/help.js
+++ commands/help.js
@@ -6,6 +6,9 @@ module.exports = {
 ${client.prefix}truth -> to get a truth question
 ${client.prefix}dare -> to get a dare
 ${client.prefix}help -> to view this message
+
+**Made by:** ArmoredVortex#8513
+**Contributers:** Devansh#6106, Adya Acharya#0532 
         `)
     }
 }
